4bbfb94ab0bf01c4adcc8e91e32cb9d59d70bc2f
Johnny Girault <esbo@hotmail.fr>
2019-12-28 23:27:43 +0100

    scss px to vh

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 
-import { check_if_in_view } from '../components/slideleft';
-check_if_in_view();
+// import { check_if_in_view } from '../components/slideleft';
+// check_if_in_view();
